79df6965a1910ea39eb8a2e12305638907bb85f9
cetinarslan <cetinarslan.se@gmail.com>
2022-08-10 16:54:20 +0200

    Added subcomponent folders

--- expensetable/src/App.js
+++ expensetable/src/App.js
@@ -1,36 +1,34 @@
-import Expenses from './components/Expenses';
+import Expenses from "./components/Expenses/Expenses";
+import "./App.css";
 
-function App() {
+const App = () => {
   const expenses = [
     {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: '€20',
+      id: "e1",
+      title: "Toilet Paper",
+      amount: "€20",
       date: new Date(2022, 0, 0),
     },
-    { id: 'e2', 
-      title: 'New TV', 
-      amount: '€500', 
-      date: new Date(2022, 1, 1) },
+    { id: "e2", title: "New TV", amount: "€500", date: new Date(2022, 1, 1) },
     {
-      id: 'e3',
-      title: 'Car Inspection',
-      amount: '€50',
+      id: "e3",
+      title: "Car Inspection",
+      amount: "€50",
       date: new Date(2022, 2, 2),
     },
     {
-      id: 'e4',
-      title: 'New Laptop',
-      amount: '€1000',
+      id: "e4",
+      title: "New Laptop",
+      amount: "€1000",
       date: new Date(2022, 3, 3),
     },
   ];
   return (
     <div>
-      <h2>Cetin ARSLAN's Expense Table</h2>
+      <h2 className="center">Cetin ARSLAN's Expense Table</h2>
       <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
