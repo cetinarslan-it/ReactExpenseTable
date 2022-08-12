245cf39d8165970764e891d80f97feddc0faf911
cetinarslan <cetinarslan.se@gmail.com>
2022-08-12 23:57:51 +0200

    Added from child to parent data transfer

--- expensetable/src/App.js
+++ expensetable/src/App.js
@@ -1,35 +1,49 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import NewExpense from './components/NewExpense/NewExpense';
-import Expenses from './components/Expenses/Expenses';
-import "./App.css";
+import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
+import './App.css';
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "New PC",
+    amount: 1000,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: "e2",
+    title: "New TV",
+    amount: 750,
+    date: new Date(2021, 2, 12),
+  },
+  {
+    id: "e3",
+    title: "Car Inspection",
+    amount: 50,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Laptop",
+    amount: 800,
+    date: new Date(2021, 5, 12),
+  },
+];
 
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: "€20",
-      date: new Date(2022, 0, 0),
-    },
-    { id: "e2", title: "New TV", amount: "€500", date: new Date(2022, 1, 1) },
-    {
-      id: "e3",
-      title: "Car Inspection",
-      amount: "€50",
-      date: new Date(2022, 2, 2),
-    },
-    {
-      id: "e4",
-      title: "New Laptop",
-      amount: "€1000",
-      date: new Date(2022, 3, 3),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+
   return (
-    <div className='main-page'>
-      <h2 className="center">Cetin's Expense Table</h2>
-      <NewExpense/>
+    <div>
+      <h1 className='center'>Cetin's Expense Table</h1>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
